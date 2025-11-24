86ce33028d490588516ee58e1c94dc74fe69bbb5
kimsha-it <kimsha0it@gmail.com>
2025-11-24 10:25:08 +0900

    read 기능 구현

---
+++ src/main/java/com/example/board/controller/PostController.java
@@ -0,0 +1,33 @@
+package com.example.board.controller;
+
+import com.example.board.dto.PostDto;
+import com.example.board.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/posts")
+@RequiredArgsConstructor
+public class PostController {
+    private final PostRepository postRepository;
+
+    @GetMapping
+    public String list(Model model) {
+        model.addAttribute("posts", postRepository.findAll());
+        return "posts/list";
+    }
+
+    @GetMapping("/{id}")
+    public String detail(@PathVariable Long id, Model model) {
+        PostDto post = postRepository.findById(id);
+        model.addAttribute("post", post);
+        return "posts/detail";
+    }
+
+
+
+}
---
+++ src/main/java/com/example/board/repository/PostRepository.java
@@ -0,0 +1,39 @@
+package com.example.board.repository;
+
+import com.example.board.dto.PostDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class PostRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    private final RowMapper<PostDto> rowMapper = (rs, rowNum) -> {
+        return new PostDto(
+                rs.getLong("id"),
+                rs.getString("title"),
+                rs.getString("content"),
+                rs.getTimestamp("create_at").toLocalDateTime()
+        );
+    };
+
+    // 전체 조회
+    public List<PostDto> findAll() {
+        String sql = "SELECT * FROM post";
+        return jdbcTemplate.query(sql, rowMapper);
+    }
+
+    public PostDto findById(Long id) {
+        String sql = "SELECT * FROM post WHERE id = ?";
+
+        // queryForObject => 단일 행 조회
+        PostDto post = jdbcTemplate.queryForObject(sql, rowMapper, id);
+
+        return post;
+    }
+}

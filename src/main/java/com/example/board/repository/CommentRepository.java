593145008841330824da6f5926f1239081b837e0
kimsha-it <kimsha0it@gmail.com>
2025-11-26 09:24:17 +0900

    Repository, Service, Controller 기본구조 작성

---
+++ src/main/java/com/example/board/repository/CommentRepository.java
@@ -0,0 +1,7 @@
+package com.example.board.repository;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CommentRepository {
+}
---
+++ src/main/java/com/example/board/service/CommentService.java
@@ -0,0 +1,14 @@
+package com.example.board.service;
+
+import com.example.board.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CommentService {
+    private final CommentRepository commentRepository;
+
+}
